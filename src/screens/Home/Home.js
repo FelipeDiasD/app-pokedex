62b357bc5c5dae5da2a90138cfb7487031176197
FelipeDiasD <prado.dias@hotmail.com>
2022-04-26 16:46:41 -0300

    Implementing ReactNavigation

---
+++ src/screens/Home/Home.js
@@ -0,0 +1,21 @@
+
+import React, { useEffect } from 'react';
+import { useState} from 'react';
+import { Alert, StyleSheet, Text, View, Image, Button, ScrollView, StatusBar} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native'
+
+
+
+export default function Home () {
+
+    return(
+        <View>
+            <Text>
+                Home
+            </Text>
+        </View>
+    )
+
+
+
+}
